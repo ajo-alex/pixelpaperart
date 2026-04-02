82dfd92b9ade2d6e922e330b32542b6b86ae7fd9
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2026-04-02 17:29:04 +0530

    firebase integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
+import Editor from './components/Editor'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import Spotlight from './components/Spotlight'
+import { useEffect, useState } from 'react'
+import { db, firebaseApp } from './lib/firebase'
+import { collection, getDocs } from 'firebase/firestore'
 
 const App = () => {
+  const [arts, setArts] = useState<{id:string,name:string,cover:string,code: string}[]>([])
+useEffect(() => {
+  const fetchArts = async () => {
+    try {
+      const artsRef = collection(db, "art");
+      const snapshot = await getDocs(artsRef);
+
+      const artsData = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      
+      setArts(artsData as {id:string,name:string,cover:string,code: string}[]);
+    } catch (error) {
+      console.error("Error fetching arts:", error);
+    }
+  };
+
+  fetchArts();
+}, []);
+
+console.log(import.meta.env.TEST);
   return (
     <div>
       {/* <Viewer /> */}
@@ -12,10 +38,10 @@ const App = () => {
       <div className="container mx-auto w-[calc(100%-2rem)] md:w-full overflow-hidden sm:overflow-visible">
         <input type="text" placeholder='Search art.....' className='p-4 border w-full mt-8 shadow-[8px_8px_0px_rgba(0,0,0,1)] outline-0' />
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-8 mb-8'>
-        {[1,2,3,4,5,6,7,8,9,10].map((item) => {
-          return <div key={item} className='w-full cursor-pointer bg-amber-300 shadow-[8px_8px_0px_rgba(0,0,0,1)] flex flex-col'>
-            <img src="https://images.pexels.com/photos/1183992/pexels-photo-1183992.jpeg" alt="" className="w-full object-cover" />
-            <div className='p-4 text-2xl font-semibold'>Iron Man</div>
+        {arts.map((item) => {
+          return <div key={item.id} className='w-full  cursor-pointer bg-amber-300 shadow-[8px_8px_0px_rgba(0,0,0,1)] flex flex-col'>
+            <img src={item.cover} alt="" className="w-full object-cover h-[300px]" />
+            <div className='p-4 text-2xl font-semibold'>{item.name}</div>
           </div>
         })}
       </div>
--- src/components/Spotlight.tsx
+++ src/components/Spotlight.tsx
@@ -6,6 +6,7 @@ const Spotlight = () => {
         <div className='text-3xl lg:text-4xl font-semibold'>Create your own pixel art</div>
        <div className='mt-4'>This is a curated collection of pixel paper art created by artists from around the world. Explore a wide variety of designs and use them as inspiration to create your own pixel art with ease.
 </div>
+<button className='mt-4 bg-amber-400 p-4 shadow-[8px_8px_0px_rgba(0,0,0,1)] cursor-pointer'>Create your own pixel art</button>
       </div>
       <div className='w-full md:w-1/2 flex justify-center md:justify-end'>
         <img src="https://i.ibb.co/RG8Dfsgp/Gemini-Generated-Image-fk6s60fk6s60fk6s.png" className="shadow-[8px_8px_0px_rgba(0,0,0,1)] max-h-[300px] md:max-h-none object-contain" alt="Pixel Art" />
